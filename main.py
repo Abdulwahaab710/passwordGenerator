c69f9adba73b3a20615fa50a2d0d44419320c4cb
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-04-13 15:00:07 -0400

    Added toInt function

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     print "Welcome to the password generator!"
     length = getLength()
     contains = raw_input("Choice from the following what should the password contains \n1)Uppercase letters\n2)Lowercase letters\n3)Numbers\n4)Symbols\nInput can be 1234>> ")
-    print list(contains)
+    contains = list(contains)
+    toInt(contains)
+    contains.sort()
+    print contains
     password = raw_input("Enter a word: ")
     print createPassword(length,contains,password)
 
@@ -15,6 +18,10 @@ def createPassword(length,contains,password):
     password = password.replace("","")
     password = password.replace("","")
     return password
+
+def toInt(lst):
+    for i in range(len(lst)):
+        lst[i] = int(lst[i])
 
 def getLength():
     length = 0
