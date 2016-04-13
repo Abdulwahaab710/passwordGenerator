052adbc12f12becb27e4ef29cddfb8eec076864f
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-04-13 14:43:59 -0400

    Added an exception for the input

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 def main():
     print "Welcome to the password generator!"
-    try:
-        length = input("Please Enter the length of the password you want to enter: ")
-    except NameError:
-        length = input("*This input accepts only numbers\nPlease Enter the length of the password you want to enter: ")
+    getLength()
     contains = raw_input("Please Enter what should the password contains :")
     password = raw_input("Enter a word: ")
     print createPassword(password)
@@ -16,6 +13,15 @@ def createPassword(password):
     password = password.replace("","")
     password = password.replace("","")
     return password
+
+def getLength():
+    try:
+        length = input("Please Enter the length of the password you want to enter: ")
+    except NameError:
+        print "*This input accepts only numbers"
+        getLength()
+    finally:
+        return length
 
 if __name__=='__main__':
     main()
