1ca9eddc720d4069a230d2e01708238b9de4dc4c
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-04-13 14:57:24 -0400

    Updated the main function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def main():
     print "Welcome to the password generator!"
-    length = 0
     length = getLength()
-    contains = raw_input("Please Enter what should the password contains :")
+    contains = raw_input("Choice from the following what should the password contains \n1)Uppercase letters\n2)Lowercase letters\n3)Numbers\n4)Symbols\nInput can be 1234>> ")
+    print list(contains)
     password = raw_input("Enter a word: ")
     print createPassword(length,contains,password)
+
 def createPassword(length,contains,password):
     password = password.replace("A","4")
     password = password.replace("a","@")
